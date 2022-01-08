ef1dd5530d36165bde39c98a2766833f2d0cecde
jitinchekka <jitinchekka2@gmail.com>
2022-01-08 13:54:43 +0530

    removed unnecessary import

--- lib/screens/farmer_homepage.dart
+++ lib/screens/farmer_homepage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dotslash5/screens/farmer_sell.dart';
 import 'package:dotslash5/screens/farmer_scan.dart';
